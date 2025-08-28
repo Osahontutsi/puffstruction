34f5578b42c222b70d79f015e37b176eec008a74
Osahontutsi <91732662+Osahontutsi@users.noreply.github.com>
2025-08-28 06:54:49 -0700

    App.js

---
+++ App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Login from "./components/Login";
+import Search from "./components/Search";
+import CommentBox from "./components/CommentBox";
+
+function App() {
+  return (
+    <div style={{ padding: "20px" }}>
+      <h2>🔥 Deliberately Vulnerable React App 🔥</h2>
+      <Login />
+      <Search />
+      <CommentBox />
+    </div>
+  );
+}
+
+export default App;
